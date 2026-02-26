9130042387d7f74d542d857a10c0a679d6b3c944
elifKemik <eliffkemik@gmail.com>
2026-02-26 11:34:30 +0300

    Final Touch: Sistem cikislari eklendi ve proje tamamlandi

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Dashboard v1.0
-<<<<<<< HEAD
 import news
-=======
 import weather
->>>>>>> 7ae816f437b6ae630925c1c474799d200a278992
+print(weather.get_info())
+print(news.get_info())
+print("Sistem başarıyla sonlandırıldı.")
